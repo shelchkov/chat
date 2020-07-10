cbcc6c3ae8d47d4639085503801e3e7921a8c0aa
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2020-07-10 11:53:09 +0400

    Add messages logic #15

---
+++ api/src/messages/messages.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Post, Param, Body } from "@nestjs/common"
+import { MessagesService } from "./messages.service"
+import { SendMessageDto } from "./dto/sendMessage.dto"
+import { GetMessagesDto } from "./dto/getMessages.dto"
+import { MessageDto } from "./dto/message.dto"
+
+@Controller("messages")
+export class MessagesController {
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @Get(":id")
+  getMessagesFromUser(
+    @Param("id") id: string,
+    @Body() body: GetMessagesDto,
+  ): MessageDto[] {
+    const { userId } = body
+    return this.messagesService.getMessagesFromUser(
+      Number(id),
+      Number(userId),
+    )
+  }
+
+  @Post(":to")
+  sendMessageToUser(
+    @Param("to") to: string,
+    @Body() message: SendMessageDto,
+  ): MessageDto {
+    return this.messagesService.sendMessageToUser(Number(to), message)
+  }
+}
