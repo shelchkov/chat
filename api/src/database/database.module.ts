ac01d0380369968181b2f268560d1347aca87f7b
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2020-07-20 14:36:20 +0400

    Fix dependencies and database configuration #38

--- api/src/database/database.module.ts
+++ api/src/database/database.module.ts
@@ -8,23 +8,32 @@ import { ConfigModule, ConfigService } from "@nestjs/config"
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
-        const type = "postgres" as any
-        const host = process.env.DB_HOST || configService.get("POSTGRES_HOST")
-        const port = process.env.DB_PORT || configService.get("POSTGRES_PORT")
-        const username = process.env.DB_USER || configService.get("POSTGRES_USER")
-        const password = process.env.DB_PASSWORD || configService.get("POSTGRES_PASSWORD")
-        const database = process.env.DB_DATABASE || configService.get("POSTGRES_DB")
+        if (!process.env.DATABASE_URL) {
+          return {
+            type: "postgres" as any,
+            host: configService.get("POSTGRES_HOST"),
+            port: configService.get("POSTGRES_PORT"),
+            username: configService.get("POSTGRES_USER"),
+            password: configService.get("POSTGRES_PASSWORD"),
+            database: configService.get("POSTGRES_DB") as string,
+            entities: [__dirname + "/../**/*.entity.{ts,js}"],
+            synchronize: true,
+          }
+        }
+
+        const urlParams = process.env.DATABASE_URL.split(":")
 
         return {
-        type,
-        host,
-        port,
-        username,
-        password,
-        database,
-        entities: [__dirname + "/../**/*.entity.{ts,js}"],
-        synchronize: true,
-      }}
+          type: "postgres" as any,
+          host: urlParams[2].split("@")[1],
+          port: urlParams[3].split("/")[0],
+          username: urlParams[1].slice(2),
+          password: urlParams[2].split("@")[0],
+          database: urlParams[3].split("/")[1],
+          entities: [__dirname + "/../**/*.entity.{ts,js}"],
+          synchronize: true,
+        }
+      },
     }),
   ],
 })
