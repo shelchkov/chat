36344179fc8a93b4f3f7d72c5896adf91c25f469
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2020-07-20 12:08:17 +0400

    Initial commit

--- api/src/database/database.module.ts
+++ api/src/database/database.module.ts
@@ -7,16 +7,24 @@ import { ConfigModule, ConfigService } from "@nestjs/config"
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: "postgres",
-        host: configService.get("POSTGRES_HOST"),
-        port: configService.get("POSTGRES_PORT"),
-        username: configService.get("POSTGRES_USER"),
-        password: configService.get("POSTGRES_PASSWORD"),
-        database: configService.get("POSTGRES_DB"),
+      useFactory: (configService: ConfigService) => {
+        const type = "postgres" as any
+        const host = process.env.DB_HOST || configService.get("POSTGRES_HOST")
+        const port = process.env.DB_PORT || configService.get("POSTGRES_PORT")
+        const username = process.env.DB_USER || configService.get("POSTGRES_USER")
+        const password = process.env.DB_PASSWORD || configService.get("POSTGRES_PASSWORD")
+        const database = process.env.DB_DATABASE || configService.get("POSTGRES_DB")
+
+        return {
+        type,
+        host,
+        port,
+        username,
+        password,
+        database,
         entities: [__dirname + "/../**/*.entity.{ts,js}"],
         synchronize: true,
-      }),
+      }}
     }),
   ],
 })
