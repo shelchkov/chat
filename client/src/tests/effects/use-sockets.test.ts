548395df7f37aaef5c2f7361d1956d61b5e2f5c3
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2020-12-27 17:38:13 +0400

    Add tests for useSockets hook #81

--- client/src/tests/effects/use-sockets.test.ts
+++ client/src/tests/effects/use-sockets.test.ts
@@ -4,7 +4,10 @@ import { useSockets } from "../../effects/use-sockets"
 
 describe("useSockets hook", (): void => {
 	let WebSocketSpy: jest.SpyInstance
-	let currentWs: { onmessage?: (event: MessageEvent) => void } = {}
+	let currentWs: {
+		onmessage?: (event: MessageEvent) => void
+		onclose?: () => void
+	} = {}
 	const close = jest.fn()
 
 	beforeAll((): void => {
@@ -55,6 +58,25 @@ describe("useSockets hook", (): void => {
 			expect(result.current.data).toEqual(newMessage)
 			expect(WebSocketSpy).toBeCalledTimes(1)
 			expect(close).toBeCalledTimes(1)
+		})
+	})
+
+	describe("when connection is closing", (): void => {
+		it("should reconnect", async (): Promise<void> => {
+			const { result, waitForNextUpdate } = renderHook(() =>
+				useSockets(),
+			)
+			expect(WebSocketSpy).toBeCalledTimes(1)
+
+			act((): void => {
+				if (currentWs.onclose) {
+					currentWs.onclose()
+				}
+			})
+
+			expect(close).toBeCalledTimes(2)
+			await waitForNextUpdate()
+			expect(WebSocketSpy).toBeCalledTimes(2)
 		})
 	})
 })
