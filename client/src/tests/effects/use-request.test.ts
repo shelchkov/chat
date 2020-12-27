b2f9d61417d2513c7ab1a39c89eaf3c6d91c5fd4
Yaroslav Shelchkov <yaroslav.shelckov@bravedevelopers.com>
2020-12-27 11:40:01 +0400

    Add tests for useRequest hook #81

--- client/src/tests/effects/use-request.test.ts
+++ client/src/tests/effects/use-request.test.ts
@@ -5,14 +5,19 @@ import { useRequest } from "../../effects/use-request"
 describe("useRequest hook", (): void => {
 	const url = "https://url.com/"
 	const data = { data: "data" }
+	let fetchSpy: jest.SpyInstance
 
-	beforeAll((): void => {
-		jest
+	beforeEach((): void => {
+		fetchSpy = jest
 			.spyOn(global, "fetch")
 			.mockImplementation(
 				() =>
 					Promise.resolve({ json: () => Promise.resolve(data) }) as any,
 			)
+	})
+
+	afterEach((): void => {
+		fetchSpy.mockClear()
 	})
 
 	describe("when initialized", (): void => {
@@ -32,7 +37,7 @@ describe("useRequest hook", (): void => {
 				useRequest({ url }),
 			)
 
-			act(() => {
+			act((): void => {
 				result.current.start()
 			})
 
@@ -41,6 +46,80 @@ describe("useRequest hook", (): void => {
 			await waitForNextUpdate()
 			expect(result.current.data).toEqual(data)
 			expect(result.current.isLoading).toEqual(false)
+			expect(result.current.error).toEqual(undefined)
+		})
+	})
+
+	describe("when resetData is called", (): void => {
+		it("should reset data", async (): Promise<void> => {
+			const { result, waitForNextUpdate } = renderHook(() =>
+				useRequest({ url }),
+			)
+
+			act((): void => {
+				result.current.start()
+			})
+
+			await waitForNextUpdate()
+			expect(result.current.data).toEqual(data)
+
+			act((): void => {
+				result.current.resetData()
+			})
+
+			expect(result.current.data).toEqual(undefined)
+			expect(result.current.isLoading).toEqual(false)
+			expect(result.current.error).toEqual(undefined)
+			expect(global.fetch).toBeCalledTimes(1)
+		})
+	})
+
+	describe("when error is thrown", (): void => {
+		const errorMessage = "Error message"
+
+		beforeEach((): void => {
+			fetchSpy.mockImplementation(() =>
+				Promise.reject({ message: errorMessage }),
+			)
+		})
+
+		it("should save error message", async (): Promise<void> => {
+			const { result, waitForNextUpdate } = renderHook(() =>
+				useRequest({ url }),
+			)
+
+			act((): void => {
+				result.current.start()
+			})
+
+			expect(result.current.isLoading).toEqual(true)
+			await waitForNextUpdate()
+			expect(result.current.error).toEqual(errorMessage)
+			expect(result.current.isLoading).toEqual(false)
+			expect(result.current.data).toEqual(undefined)
+			expect(global.fetch).toBeCalledTimes(1)
+		})
+	})
+
+	describe("when start called two times in a row", (): void => {
+		it("should call fetch only once", async (): Promise<void> => {
+			const { result, waitForNextUpdate } = renderHook(() =>
+				useRequest({ url }),
+			)
+
+			act((): void => {
+				result.current.start()
+			})
+
+			expect(result.current.isLoading).toEqual(true)
+
+			act((): void => {
+				result.current.start()
+			})
+
+			await waitForNextUpdate()
+			expect(global.fetch).toBeCalledTimes(1)
+			expect(result.current.data).toEqual(data)
 		})
 	})
 })
