121e8e780f3371915d5d150eac328bd7142d0fdc
Yaroslav Shelchkov <shelchkovy@yandex.ru>
2021-09-21 08:08:27 +0400

    feat(web): add users list hover effect #79

--- client/src/style-guide/theme.ts
+++ client/src/style-guide/theme.ts
@@ -6,7 +6,7 @@ interface Theme {
 export const theme: Theme = {
 	breakpoints: ["30rem", "45rem"],
 	colors: {
-		greys: ["#333", "#ccc", "#c7c7c7"],
+		greys: ["#333", "#ccc", "#c7c7c7", "#f6f6f6"],
 		greens: ["limegreen"],
 	},
 }
