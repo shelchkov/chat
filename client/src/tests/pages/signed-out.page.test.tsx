a29b72a5e5a9454204d3ea1cc99601ca15b1da8c
Yaroslav Shelchkov <shelchkovy@yandex.ru>
2021-09-25 10:52:12 +0400

    feat(web): add tests #120

--- client/src/tests/pages/signed-out.page.test.tsx
+++ client/src/tests/pages/signed-out.page.test.tsx
@@ -23,27 +23,37 @@ describe("signed out page", (): void => {
 		}
 
 		it("changes switch text", (): void => {
-			expect(signedOutPage.contains(createAccount)).toEqual(true)
-			expect(signedOutPage.contains(signIn)).toEqual(false)
+			expect(signedOutPage.contains(createAccount)).toBeTruthy()
+			expect(signedOutPage.contains(signIn)).toBeFalsy()
 
 			clickSwitch()
 
-			expect(signedOutPage.contains(createAccount)).toEqual(false)
-			expect(signedOutPage.contains(signIn)).toEqual(true)
+			expect(signedOutPage.contains(createAccount)).toBeFalsy()
+			expect(signedOutPage.contains(signIn)).toBeTruthy()
+
+			clickSwitch()
+
+			expect(signedOutPage.contains(createAccount)).toBeTruthy()
+			expect(signedOutPage.contains(signIn)).toBeFalsy()
 		})
 
 		it("changes form", (): void => {
-			expect(signedOutPage.find(SignInForm).length).toEqual(1)
-			expect(signedOutPage.find(SignUpForm).length).toEqual(0)
+			expect(signedOutPage.find(SignInForm).exists()).toBeTruthy()
+			expect(signedOutPage.find(SignUpForm).exists()).toBeFalsy()
 
 			clickSwitch()
 
-			expect(signedOutPage.find(SignInForm).length).toEqual(0)
-			expect(signedOutPage.find(SignUpForm).length).toEqual(1)
+			expect(signedOutPage.find(SignInForm).exists()).toBeFalsy()
+			expect(signedOutPage.find(SignUpForm).exists()).toBeTruthy()
+
+			clickSwitch()
+
+			expect(signedOutPage.find(SignInForm).exists()).toBeTruthy()
+			expect(signedOutPage.find(SignUpForm).exists()).toBeFalsy()
 		})
 	})
 
 	it("should contain signed out text", (): void => {
-		expect(signedOutPage.contains(signedOutText)).toEqual(true)
+		expect(signedOutPage.contains(signedOutText)).toBeTruthy()
 	})
 })
