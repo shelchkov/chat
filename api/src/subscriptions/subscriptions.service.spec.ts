a2fd972ff26b1aae2e76f796afcf538ef5fa510e
Yaroslav Shelchkov <shelchkovy@yandex.ru>
2022-01-09 09:42:24 +0400

    feat(api): add tests #70

--- api/src/subscriptions/subscriptions.service.spec.ts
+++ api/src/subscriptions/subscriptions.service.spec.ts
@@ -1,9 +1,22 @@
 import { Test } from "@nestjs/testing"
 import { UsersService } from "../users/users.service"
 import mockedUser from "../utils/mocks/user"
-import { copy } from "../utils/utils"
+import * as utils from "../utils/utils"
 import { AuthenticationService } from "../authentication/authentication.service"
 import { SubscriptionsService } from "./subscriptions.service"
+
+jest.mock("../utils/utils", () => {
+  const utils = jest.requireActual("../utils/utils")
+
+  return {
+    ...utils,
+    getCookieParams: jest
+      .fn()
+      .mockReturnValue({ Authentication: "authenticationToken" }),
+  }
+})
+
+const { copy } = utils
 
 describe("SubscriptionsService", () => {
   let subscriptionsService: SubscriptionsService
@@ -31,13 +44,30 @@ describe("SubscriptionsService", () => {
     )
   })
 
-  describe("when handling error", () => {
-    it("should send error and disconnect", () => {
-      const client = { send: jest.fn(), disconnect: jest.fn() } as any
+  const send = jest.fn()
+  const disconnect = jest.fn()
+  const client = { send, disconnect } as any
+  
+  describe("when handling connection", () => {
+    const request = { headers: { cookie: "cookie" } } as any
 
-      subscriptionsService.sendErrorAndDisconnect(client)
-      expect(client.send).toBeCalledTimes(1)
-      expect(client.disconnect).toBeCalledTimes(1)
+    beforeEach(() => {
+      send.mockClear()
+      disconnect.mockClear()
+    })
+
+    describe("and user isn't authorized", () => {
+      it("should send error and disconnect", () => {
+        subscriptionsService.handleConnection(client, request)
+
+        expect(getUserIdFromToken).toBeCalledTimes(1)
+        expect(getUserIdFromToken).toBeCalledWith("authenticationToken")
+        expect(client.send).toBeCalledTimes(1)
+        expect(JSON.parse(client.send.mock.calls[0][0])).toHaveProperty(
+          "error",
+        )
+        expect(client.disconnect).toBeCalledTimes(1)
+      })
     })
   })
 })
